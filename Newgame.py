e1605c0b8e2dd9c9e0301cf821f9542e7f10e774
TanTianxuJaden <s10242805@connect.np.edu.sg>
2023-12-05 22:14:46 +0800

    Update on board

--- Newgame.py
+++ Newgame.py
@@ -5,7 +5,7 @@ def start_new_game():
     empty_space = '□'
 
     # Create a 20x20 board with an outline of squares
-    rows, cols = 20, 20
+    rows, cols = 22, 22
     game_board = [['□' if i == 0 or i == rows - 1 or j == 0 or j == cols - 1 else '-' for j in range(cols)] for i in range(rows)]
 
     # Print the board
